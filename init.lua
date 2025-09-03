71e9158e54ee45248d49d5d28f0409c24a20a0b5
JohnnyVenturas <bogdanbudura@gmail.com>
2025-09-03 16:05:59 +0300

    adding cmp, and mason

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require('config.basic')
 require('config.lazy')
 require('plugins.catppuccin')
+require('plugins.cmp')
 
 
 
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,79 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip'
+    },
+    config = function()
+        -- Configure diagnostics
+        vim.diagnostic.config({
+            virtual_text = false, -- Disable inline virtual text
+            signs = true,         -- Show signs in the sign column
+            underline = false,     -- Underline diagnostic text
+            update_in_insert = true,
+            severity_sort = true,
+            float = {
+                border = 'rounded',
+                source = 'always',  -- Show diagnostic source
+                header = '',
+                prefix = '',
+                focusable = true,   -- Make float window focusable
+            }
+        })
+
+        -- Set up diagnostic navigation keymaps
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic in float window' })
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Open diagnostic quickfix list' })
+
+        -- nvim-cmp setup
+        local cmp = require'cmp'
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, 
+                { name = 'buffer' }
+            })
+        })
+        
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+        
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                { name = 'cmdline' }
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
+    end
+}
+
