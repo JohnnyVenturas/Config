701c1d2e2e10229628ef56aa88cbbc1951e47668
JohnnyVenturas <bogdanbudura@gmail.com>
2025-09-15 02:42:00 +0200

    making install.sh robust

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-if [ ! -d ~/.config/nvim ]; then
+target_dir=~/.config/nvim
+if [ ! -d "$target_dir" ] ; then
 	mkdir -p ~/.config/nvim
 fi
 
 
+#persist the folder 
+cp  *.lua "$target_dir"
 
-cp  *.lua ~/.config/nvim
+cp -r lua "$target_dir"
 
-cp -r lua ~/.config/nvim
+cp install.sh "$target_dir"
+
+#setup git folder for future use 
+[ -f "$target_dir"/.gitignore ] && rm "$target_dir"/.gitignore ; cp .gitignore "$target_dir"
+
+[ -d "$target_dir"/.git ]  && rm -rf "$target_dir"/.git ; cp .git "$target_dir"
+
+
+
+
