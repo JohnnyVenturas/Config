6d94aa82a62941bb35764106a83e3c5b6f260562
JohnnyVenturas <bogdanbudura@gmail.com>
2025-09-19 11:07:32 +0200

    adding install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,12 @@
 target_dir=~/.config/nvim
-if [ ! -d "$target_dir" ] ; then
-	mkdir -p ~/.config/nvim
+if [ -e "$target_dir" ] ; then
+    echo "$target_dir already exist."
+    echo "run rm -rf $target_dir"
 fi
 
+#symbolic link to the nvim folder
+ln -s $PWD $target_dir
 
-#persist the folder 
-cp  *.lua "$target_dir"
-
-cp -r lua "$target_dir"
-
-cp install.sh "$target_dir"
-
-#setup git folder for future use 
-[ -f "$target_dir"/.gitignore ] && rm "$target_dir"/.gitignore ; cp .gitignore "$target_dir"
-
-[ -d "$target_dir"/.git ]  && rm -rf "$target_dir"/.git ; cp .git "$target_dir"
-
-
-
+#symbolic link to .tmux.conf
+ln -s .tmux.conf ~/.tmux.conf
 
