c067b81f9d03f30e0e67bfb7f0ce6c4279d046af
JohnnyVenturas <bogdanbudura@gmail.com>
2026-01-08 13:07:48 +0100

    improving install.sh. Adding docs. Adding git ignore

--- install.sh
+++ install.sh
@@ -11,17 +11,17 @@ newline() {
     printf '\n%.0s' $(seq 1 2)
 }
 
-zshenv() {
-
-    if [ -e "$HOME/.zshenv" ]; then
-        printf "Please remmove zshenv before proceding"
-    else
-        ln -s "$PWD/.zshenv" "$HOME/.zshenv"
-    fi
-}
+# zshenv() {
+#
+#     if [ -e "$HOME/.zshenv" ]; then
+#         printf "Please remmove zshenv before proceding"
+#     else
+#         ln -s "$PWD/.zshenv" "$HOME/.zshenv"
+#     fi
+# }
 
 set_base_paths () {
-    base_directory="$HOME/.config"
+    config_directory="$HOME/.config"
 }
 
 build_parents() {
@@ -29,41 +29,99 @@ build_parents() {
     set -- "tmux" "zsh" "wezterm"
 
     for parent_directory in "$@"; do
-        if [ ! -e "$base_directory/$parent_directory" ]; then
-            mkdir "$base_directory/$parent_directory"
-            printf "%bCreating%b parent_directory %b%s%b" "$RED" "$NC"  "$GREEN" "$base_directory/$parent_directory" "$NC"
+        if [ ! -e "$config_directory/$parent_directory" ]; then
+            mkdir "$config_directory/$parent_directory"
+            printf "%bCreating%b parent_directory %b%s%b" "$RED" "$NC"  "$GREEN" "$config_directory/$parent_directory" "$NC"
         fi
     done
 
 }
 
-config_files() {
-    set -- "nvim" "tmux/tmux.conf" ".zprofile" ".profile" "zsh/.zshrc" ".bashrc" "wezterm/wezterm.lua"
+install_configs() {
+    set -- "nvim" "tmux/tmux.conf" "zsh/.zprofile"  "zsh/.zshrc" "zsh/.zshenv" ".profile" ".bashrc" "wezterm/wezterm.lua"
     success=0
     failure=0
     total=$#
 
     for configuration_file in "$@"; do
-        if [ -e "$base_directory/$configuration_file" ]; then
-            printf "%s already exists\n"  "$base_directory/$configuration_file"
+        source_path="$PWD/$configuration_file"
+
+        case $configuration_file in
+            "zsh/.zshenv")
+                destination_path="$HOME/.zshenv"
+                ;;
+            *)
+                destination_path="$config_directory/$configuration_file"
+                ;;
+        esac
+
+
+        if [ -e $destination_path ]; then
+            printf "%s already exists\n"  "$destination_path"
             printf "%bFailiure%b . Remove old config before continuing (e.g $configuration_file)\n" "${RED}" "${NC}"
             failure=$((failure + 1))
             newline
         else
-            printf "%bSuccess%b . Linked $base_directory/$configuration_file to %s.\n" "${GREEN}" "${NC}" "$PWD/$configuration_file"
+            printf "%bSuccess%b . Linked %s  to %s.\n" "${GREEN}" "${NC}" "$destination_path" "$source_path"
             success=$((success + 1))
-            ln -s "$PWD/$configuration_file" "$base_directory/$configuration_file"
+            ln -s "$source_path" "$destination_path"
         fi
     done
-    printf "succeded %s .  failed %s\n" "$success/$total" "$failure/$total"
+
+    printf "%bsucceded%b %s " "${GREEN}" "${NC}" "$success/$total"  
+    printf "%bfailed%b %s\n" "${RED}" "${NC}" "$failure/$total"
 }
 
+uninstall_configs() {
+    set -- "nvim" "tmux/tmux.conf" "zsh/.zprofile"  "zsh/.zshrc" "zsh/.zshenv" ".profile" ".bashrc" "wezterm/wezterm.lua"
+
+    for configuration_file in "$@"; do
+
+        case $configuration_file in
+            "zsh/.zshenv")
+                destination_path="$HOME/.zshenv"
+                ;;
+            *)
+                destination_path="$config_directory/$configuration_file"
+                ;;
+        esac
+
+        unlink "$destination_path"
+    done
+}
 
 colors
-zshenv
-
 set_base_paths
 build_parents
-config_files
+
+parse_input() {
+
+    if [ -z "$1" ];then
+        set -- '-h'
+    fi
+
+    while [ ! -z "$1" ]; do
+        case $1 in
+            "-i"|"--install")
+                install_configs
+                shift
+                ;;
+            "-u"|"--uninstall")
+                uninstall_configs
+                shift
+                ;;
+            *)
+                printf "Find below the docs\n"
+                printf "\t-h|--help to see the docs\n"
+                printf "\t-i|--install to install all the configuration files\n"
+                printf "\t-u|--uninstall to uninstall all the configuration files\n"
+                shift
+        esac
+    done
+
+}
+
+parse_input "$@"
+
 
 
